3515ab8a5297c86109f17136d6e329ddd17a5564
Rubens Sousa <rubens.silva@darmlabs.ifce.edu.br>
2023-07-24 21:04:45 -0300

    case em java

--- src/exercicio/MesDoAno.java
+++ src/exercicio/MesDoAno.java
@@ -1,18 +1,18 @@
-//public class MesDoAno {
+public class MesDoAno {
 
-    //public static void main(String[] args) {
-      //  switch (variavelASerTestada) {
-        //case opcao1:
+    public static void main(String[] args) {
+        switch (variavelASerTestada) {
+        case opcao1:
                 // comando(s) caso a opção 1 tenha sido escolhida
-          //      break;
-        //case opcao2:
+                break;
+        case opcao2:
                 // comando(s) caso a opção 2 tenha sido escolhida
-          //      break;
-        //case opcao3:
+                break;
+        case opcao3:
                 // comando(s) caso a opção 3 tenha sido escolhida
-         //       break;
-        //default:
+                break;
+        default:
                 // comando(s) caso nenhuma das opções anteriores tenha sido escolhida
-//    }
-//    }
-//}
+    }
+    }
+}
